1c837f949e29aca5f25cf99466bc88491751dd97
ericazpcalaca <ericazpc@gmail.com>
2023-03-08 14:35:11 -0800

    Insert the bottom menu on the Customer Main without code

--- app/src/main/java/com/example/smart_meal/LoginActivity.java
+++ app/src/main/java/com/example/smart_meal/LoginActivity.java
@@ -33,17 +33,17 @@ public class LoginActivity extends AppCompatActivity {
                 String password = pass.getText().toString();
 
                 //checking if user exists and validating with db, simple validation to prevent empty fields
-                if (user.equals("") || password.equals(""))
-                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-                else {
-                    Boolean checkUser = DB.checkUser(DBHelper.COLUMN_EMAIL_CUST, DBHelper.COLUMN_EMAIL_BUS);
-                    Boolean checkUserPassword = DB.checkUserPassword(DBHelper.COLUMN_EMAIL_CUST, DBHelper.COLUMN_PASSWORD_CUST, DBHelper.COLUMN_EMAIL_BUS, DBHelper.COLUMN_PASSWORD_BUS);
-                    if (checkUser == false || checkUserPassword == false) {
-                        startActivity(new Intent(LoginActivity.this, NewAccount.class));
-                    }
-                }
+//                if (user.equals("") || password.equals(""))
+//                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+//                else {
+//                    Boolean checkUser = DB.checkUser(DBHelper.COLUMN_EMAIL_CUST, DBHelper.COLUMN_EMAIL_BUS);
+//                    Boolean checkUserPassword = DB.checkUserPassword(DBHelper.COLUMN_EMAIL_CUST, DBHelper.COLUMN_PASSWORD_CUST, DBHelper.COLUMN_EMAIL_BUS, DBHelper.COLUMN_PASSWORD_BUS);
+//                    if (checkUser == false || checkUserPassword == false) {
+//                        startActivity(new Intent(LoginActivity.this, NewAccount.class));
+//                    }
+//                }
                 //Está indo direto para o Business Main só irmos avançando enquanto nao termino/testo db
-                startActivity(new Intent(LoginActivity.this, BusinessMain.class));
+                startActivity(new Intent(LoginActivity.this, CustomerMain.class));
             }
         });
         //Create new account
