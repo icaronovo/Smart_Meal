e140268f8322564cbc35356982f359b108ca2aa2
ericazpcalaca <ericazpc@gmail.com>
2023-03-17 11:43:36 -0700

    Fixed the recycler view on customer main

--- app/src/main/java/com/example/smart_meal/CustomerMain.java
+++ app/src/main/java/com/example/smart_meal/CustomerMain.java
@@ -2,11 +2,15 @@ package com.example.smart_meal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -14,19 +18,25 @@ import com.google.android.material.navigation.NavigationBarView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CustomerMain extends AppCompatActivity {
+public class CustomerMain extends AppCompatActivity implements ItemAdapter.ItemClickListener{
 
     BottomNavigationView bottomNavigationView;
     ArrayList<ItemModel> itemList;
     RecyclerView recyclerView;
+    ImageView imageView;
+    ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
-
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
+        androidx.recyclerview.widget.RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
+        int numOfColumns = 1;
+        recyclerView.setLayoutManager(new GridLayoutManager(this,numOfColumns));
+        itemAdapter = new ItemAdapter(this,initData());
+        itemAdapter.setClickListener(this);
+        recyclerView.setAdapter(itemAdapter);
     }
 
     //Created a list to tryout the recyclervier
@@ -45,5 +55,10 @@ public class CustomerMain extends AppCompatActivity {
         itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
 
         return itemList;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this,"Selected space",Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/smart_meal/ItemAdapter.java
+++ app/src/main/java/com/example/smart_meal/ItemAdapter.java
@@ -1,5 +1,6 @@
 package com.example.smart_meal;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,28 +12,34 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+public class ItemAdapter extends RecyclerView.Adapter{
     List<ItemModel> itemList;
+    LayoutInflater mInflater;
+    ItemClickListener itemClickListener;
 
-    public ItemAdapter(List<ItemModel> itemList){
+    public ItemAdapter(Context context, List<ItemModel> itemList){
         this.itemList = itemList;
+        mInflater = LayoutInflater.from(context);
+    }
 
+    Integer getItem(int id){
+        return itemList.get(id).getImageItem();
     }
 
     @NonNull
     @Override
     public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_row,parent,false);
+        View view = mInflater.inflate(R.layout.recycler_view_row,parent,false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
-        holder.itemImage.setImageResource(itemList.get(position).getImageItem());
-        holder.textTitle.setText(itemList.get(position).getTitleTxt());
-        holder.textPrice.setText(itemList.get(position).getTxtPrice());
-        holder.textDesc.setText(itemList.get(position).getTxtDescItem());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ViewHolder)holder).itemImage.setImageResource(itemList.get(position).getImageItem());
+        ((ViewHolder)holder).textTitle.setText(itemList.get(position).getTitleTxt());
+        ((ViewHolder)holder).textPrice.setText(itemList.get(position).getTxtPrice());
+        ((ViewHolder)holder).textDesc.setText(itemList.get(position).getTxtDescItem());
     }
 
     @Override
@@ -40,7 +47,14 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
         return itemList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    void setClickListener(ItemClickListener mItemClickListener){
+        itemClickListener = mItemClickListener;
+    }
+
+    public interface ItemClickListener{
+        void onItemClick(View view, int position);
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView itemImage;
         TextView textTitle, textPrice, textDesc;
@@ -51,6 +65,13 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
             textTitle = itemView.findViewById(R.id.txtTitleItem);
             textPrice = itemView.findViewById(R.id.txtPriceItem);
             textDesc = itemView.findViewById(R.id.txtDescItem);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(itemClickListener != null){
+                itemClickListener.onItemClick(v,getAdapterPosition());
+            }
         }
     }
 }
