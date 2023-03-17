e20ed77b1ac1a08adfae20b40feb6adb5a0dd7ba
ericazpcalaca <ericazpc@gmail.com>
2023-03-17 11:24:57 -0700

    Start to fix up the customer main - deleted the fragments

--- app/src/main/java/com/example/smart_meal/CustomerMain.java
+++ app/src/main/java/com/example/smart_meal/CustomerMain.java
@@ -2,6 +2,7 @@ package com.example.smart_meal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +11,14 @@ import android.view.MenuItem;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerMain extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
-    CHomeFragment homeFragment = new CHomeFragment();
-    CProfileFragment profileFragment = new CProfileFragment();
+    ArrayList<ItemModel> itemList;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +26,24 @@ public class CustomerMain extends AppCompatActivity {
         setContentView(R.layout.activity_customer_main);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
 
+    }
 
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
-                        return true;
-                    case R.id.search:
-//                        Insert here the page to Search Class
-//                        startActivity(new Intent(MainActivity.this,CSearch.class));
-                        return true;
-                    case R.id.order:
-//                        Insert here the page to Order Class
-//                        startActivity(new Intent(MainActivity.this,COrder.class));
-                        return true;
-                    case R.id.profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
-                        return true;
-                }
-                return false;
-            }
-        });
+    //Created a list to tryout the recyclervier
+    //Latter figure out how to import from database :)
+    private List<ItemModel> initData(){
+        itemList = new ArrayList<>();
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+        itemList.add(new ItemModel(R.drawable.ic_launcher_background,"Title Item","$9.99","Description, description bla bla bla"));
+
+        return itemList;
     }
 }
