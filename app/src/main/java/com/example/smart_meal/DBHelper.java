e068ab27c2aa6083fd54addb7bd38e116871754d
Ícaro Novo de Oliveira <icaronovo@outlook.com>
2023-03-19 15:12:06 -0700

    Fixed the login screen and updated the databases of the app.

--- app/src/main/java/com/example/smart_meal/DBHelper.java
+++ app/src/main/java/com/example/smart_meal/DBHelper.java
@@ -10,64 +10,39 @@ import androidx.annotation.Nullable;
 public class DBHelper extends SQLiteOpenHelper {
     //creating db
     public static final String SMART_MEAL_DB = "SmartMealDB";
-    public static final int dbVersion = 8;
+    public static final int dbVersion = 10;
 
-    //creating customer table and fields
-    public static final String CUSTOMER = "CUSTOMER";
-    public static final String COLUMN_CUSTOMER_ID = "CustomerID";
-    public static final String COLUMN_EMAIL_CUST = "EmailCust";
-    public static final String COLUMN_PASSWORD_CUST = "PasswordCust";
-
-    //creating business table and fields
-    public static final String BUSINESS = "BUSINESS";
-    public static final String COLUMN_BUSINESS_ID = "BusinessID";
-    public static final String COLUMN_EMAIL_BUS = "EmailBus";
-    public static final String COLUMN_PASSWORD_BUS = "PasswordBus";
 
     //creating item table and fields
     public static final String ITEM = "ITEM";
     public static final String COLUMN_ITEM_ID = "ItemID";
-
     public static final String COLUMN_ITEM_NAME = "ItemName";
+    public static final String COLUMN_ITEM_VALUE = "ItemValue";
     public static final String COLUMN_ITEM_DESCRIPTION = "ItemDescription";
 
-    //creating order status table and fields
-    public static final String ORDER_STATUS = "ORDER_STATUS";
-    public static final String COLUMN_ORDER_ID = "OrderID";
 
     //creating order_info table and fields
     public static final String ORDER_INFO = "ORDER_INFO";
-    public static final String COLUMN_ORDER_STATUS1 = "OrderStatus";
-    public static final String COLUMN_ITEM_VALUE = "ItemValue";
+    public static final String COLUMN_ORDER_ID = "OrderID";
+    public static final String COLUMN_ORDER_STATUS = "OrderStatus";
+//    public static final String COLUMN_ITEM_ID = "ItemID";
+//    public static final String COLUMN_ITEM_VALUE = "ItemValue";
     public static final String COLUMN_ITEM_QTY = "ItemQty";
+    public static final String COLUMN_BUSINESS_ID = "BusinessID";
+    public static final String COLUMN_CUSTOMER_ID = "CustomerID";
 
-    //creating business info table and fields
-    public static final String BUSINESS_INFO = "BUSINESS_INFO";
-    public static final String COLUMN_BUSINESS_NAME = "BusinessName";
-    public static final String COLUMN_BUS_STATE_PROVINCE = "State_Province";
-    public static final String COLUMN_STATE_PROVINCE = COLUMN_BUS_STATE_PROVINCE;
-    public static final String COLUMN_BUS_CITY = "City";
-    public static final String COLUMN_BUS_ADDRESS = "Address";
-    public static final String COLUMN_PHONE_BUS = "PhoneBus";
-    //creating customer info table and fields
+
+    //creating general account info table
     public static final String CUSTOMER_INFO = "CUSTOMER_INFO";
-    public static final String COLUMN_LAST_NAME = "LastName";
-    public static final String COLUMN_FIRST_NAME = "FirstName";
-    public static final String COLUMN_PHONE_CUST = "Phone";
+    public static final String ACCOUNT_TYPE = "AccountType";
+    public static final String COLUMN_EMAIL = "EmailCust";
+    public static final String COLUMN_PASSWORD = "PasswordCust";
+    public static final String COLUMN_NAME = "Name";
+    public static final String COLUMN_PHONE = "Phone";
     public static final String COLUMN_ADDRESS = "Address";
     public static final String COLUMN_CITY = "City";
+    public static final String COLUMN_PROVINCE = "Province";
 
-
-    //public static final String DBNAMECUSTOMER = "LoginCustomer.db";
-    //public static final String DBNAMEBUSINESS = "LoginBusiness.db";
-
-    //data types in SQLITE
-    // NULL- NULL VALUE
-    //INTEGER - SIGNED INTEGER, STORED IN 1,2,3,4,6,OR 8 BYTES
-    //REAL - A FLOATING POINT VALUE
-    //TEXT - TEXT STRING
-    //BLOB- BYNARY LARGE OBJECT FOR JPG MOV MP3 BITS STORED IN THE TABLE,
-    //(ITS BETTER TO STORE IMAGES IN A SEPARATE FOLDER AND STORE ONLY THE FILE NAME IN THE DATABASE)
 
     public DBHelper(@Nullable Context context) {
         super(context, SMART_MEAL_DB, null, dbVersion);
@@ -82,117 +57,49 @@ public class DBHelper extends SQLiteOpenHelper {
                 + COLUMN_ITEM_NAME + " TEXT, "
                 + COLUMN_ITEM_DESCRIPTION + " TEXT)";
         db.execSQL(createTableItem);
-        String createTableOrderStatus = "CREATE TABLE " + ORDER_STATUS + "("
-                + COLUMN_ORDER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_ORDER_STATUS1 + " TEXT)";
-        db.execSQL(createTableOrderStatus);
+
         String createTableCustomerInfo = "CREATE TABLE " + CUSTOMER_INFO + "("
                 + COLUMN_CUSTOMER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_EMAIL_CUST + " TEXT, "
-                + COLUMN_FIRST_NAME + " TEXT, "
-                + COLUMN_LAST_NAME + " TEXT, "
-                + COLUMN_PHONE_CUST + " INTEGER, "
+                + ACCOUNT_TYPE + " TEXT, "
+                + COLUMN_EMAIL + " TEXT, "
+                + COLUMN_PASSWORD + " TEXT, "
+                + COLUMN_NAME + " INTEGER, "
+                + COLUMN_PHONE + " TEXT, "
                 + COLUMN_ADDRESS + " TEXT, "
                 + COLUMN_CITY + " TEXT, "
-                + COLUMN_STATE_PROVINCE + " TEXT, "
-                + COLUMN_PASSWORD_CUST + " TEXT)";
+                + COLUMN_PROVINCE + " TEXT)";
         db.execSQL(createTableCustomerInfo);
-        String createTableBusinessInfo = "CREATE TABLE " + BUSINESS_INFO + "("
-                + COLUMN_BUSINESS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_EMAIL_BUS + " TEXT, "
-                + COLUMN_BUSINESS_NAME + " TEXT, "
-                + COLUMN_PHONE_BUS + " INTEGER, "
-                + COLUMN_BUS_ADDRESS + " TEXT, "
-                + COLUMN_BUS_CITY + " TEXT, "
-                + COLUMN_BUS_STATE_PROVINCE + " TEXT, "
-                + COLUMN_PASSWORD_BUS + " TEXT)";
-        db.execSQL(createTableBusinessInfo);
+
         String createTableOrderInfo = "CREATE TABLE " + ORDER_INFO + "("
-                + COLUMN_ORDER_ID + " INTEGER references " + ORDER_STATUS + "(" + COLUMN_ORDER_ID + "),"
-                + COLUMN_ITEM_ID + " INTEGER references " + ITEM + "(" + COLUMN_ITEM_ID + "),"
-                + COLUMN_ITEM_QTY + " INTEGER, "
+                + COLUMN_ORDER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_ORDER_STATUS + " INTEGER , "
+                + COLUMN_ITEM_ID + " INTEGER, "
                 + COLUMN_ITEM_VALUE + " INTEGER, "
-                + COLUMN_ORDER_STATUS1 + " TEXT, "
-                + COLUMN_CUSTOMER_ID + " INTEGER, "
-                + COLUMN_BUSINESS_ID + " INTEGER)";
+                + COLUMN_ITEM_QTY + " TEXT, "
+                + COLUMN_BUSINESS_ID + " INTEGER, "
+                + COLUMN_CUSTOMER_ID + " INTEGER)";
         db.execSQL(createTableOrderInfo);
-        String createTableCustomer = " CREATE TABLE " + CUSTOMER + "("
-                + COLUMN_EMAIL_CUST + " TEXT, "
-                + COLUMN_PASSWORD_CUST + " TEXT, "
-                + COLUMN_CUSTOMER_ID + " INTEGER, "+ " FOREIGN KEY (" + COLUMN_CUSTOMER_ID + ") references "
-                + CUSTOMER_INFO + "(" + COLUMN_CUSTOMER_ID + ") "+ "ON DELETE CASCADE ON UPDATE CASCADE );";
-        db.execSQL(createTableCustomer);
-        String createTableBusiness = " CREATE TABLE " + BUSINESS + " ("
-                + COLUMN_EMAIL_BUS + " TEXT, "
-                + COLUMN_PASSWORD_BUS + " TEXT, "
-                + COLUMN_BUSINESS_ID + " INTEGER,"+ " FOREIGN KEY (" + COLUMN_BUSINESS_ID + ") references "
-                + BUSINESS_INFO + "(" + COLUMN_BUSINESS_ID + ") "+ "ON DELETE CASCADE ON UPDATE CASCADE );";
-        db.execSQL(createTableBusiness);
     }
 
     // this is called if the database version number changes. It prevents previous users apps from breaking when you change the database design.
     @Override
     //CREATING TABLES OR DROPPING IF IT EXISTS
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists CUSTOMER");
-        db.execSQL("drop table if exists BUSINESS");
-        db.execSQL("drop table if exists ITEM");
-        db.execSQL("drop table if exists ORDER_STATUS");
         db.execSQL("drop table if exists CUSTOMER_INFO");
-        db.execSQL("drop table if exists BUSINESS_INFO");
+        db.execSQL("drop table if exists ITEM");
         db.execSQL("drop table if exists ORDER_INFO");
         onCreate(db);
     }
-//    METHOD TO ADD A NEW USER, using just email and password.
-//    public boolean insertUser(String COLUMN_EMAIL_CUST, String COLUMN_PASSWORD_CUST) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues contentValues = new ContentValues();
-//        contentValues.put("username", COLUMN_EMAIL_CUST);
-//        contentValues.put("password", COLUMN_PASSWORD_CUST);
-//        long result = db.insert(CUSTOMER, null, contentValues);
-//        return result != -1;
-//    }
-//
-//    //METHOD TO ADD A NEW BUSINESS
-//    public boolean insertBusiness(String COLUMN_EMAIL_BUS, String COLUMN_PASSWORD_BUS) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues contentValues = new ContentValues();
-//        contentValues.put("username", COLUMN_EMAIL_BUS);
-//        contentValues.put("password", COLUMN_PASSWORD_BUS);
-//        long result = db.insert(BUSINESS, null, contentValues);
-//        return result != -1;
-//    }
 
-//    checking if the user already exists in the database, tried to run, query error
-//    public boolean checkUser(String EmailCust) {
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        Cursor cursor = db.rawQuery("Select * from  " +  CUSTOMER + " where  EmailCust = ? ", new String[]{EmailCust});
-//        //cursor.close();
-//        return cursor.getCount() > 0;
-//    }
-//    public boolean checkBus(String EmailBus) {
-//        SQLiteDatabase db = this.getReadableDatabase();
-//        Cursor cursor = db.rawQuery("Select * from " + BUSINESS + " where EmailBus = ? ", new String[]{EmailBus});
-//        //cursor.close();
-//        return cursor.getCount() > 0;
-//    }
-
-//    CHECKING IF PASSWORD ALREADY EXISTS IN THE DATABASE
-    public boolean checkUserPasswordCustomer (String Email, String Password) {
+    // This method us used to check if the account is of type Business or Customer
+    public Cursor checkAccountType (String Email) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("Select * from " +  CUSTOMER + " where EmailCust = ? and PasswordCust = ?", new String[]{Email, Password});
-        System.out.print("______________Cursor = " + cursor);
-        //cursor.close();
-        return cursor.getCount() > 0;
+        Cursor cursor = db.rawQuery("SELECT AccountType FROM CUSTOMER_INFO WHERE EmailCust= ?", new String[]{Email});
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
     }
-    public boolean checkUserPasswordBusiness (String EmailBus, String PasswordBus) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("Select * from " + BUSINESS + " where EmailBus = ? and PasswordBus = ?", new String[]{EmailBus, PasswordBus});
-        //cursor.close();
-        return cursor.getCount() > 0;
-    }
-
-//    public boolean checkBusinessPasssword ()
 
     //method to add customer data in the database
     public boolean addCustomer(CustomerModel customerModel) {
@@ -201,36 +108,17 @@ public class DBHelper extends SQLiteOpenHelper {
         //linking data from getters to database fields
         ContentValues cv = new ContentValues();
         //cv.put(COLUMN_CUSTOMER_ID,customerModel.getId());
-        cv.put(COLUMN_FIRST_NAME, customerModel.getFirstName());
-        cv.put(COLUMN_LAST_NAME, customerModel.getLastName());
-        cv.put(COLUMN_EMAIL_CUST, customerModel.getCustomerEmail());
-        cv.put(COLUMN_PHONE_CUST, customerModel.getCustomerPhone());
+        cv.put(ACCOUNT_TYPE, customerModel.getAccountType());
+        cv.put(COLUMN_EMAIL, customerModel.getCustomerEmail());
+        cv.put(COLUMN_PASSWORD, customerModel.getPassword());
+        cv.put(COLUMN_NAME, customerModel.getCustomerName());
+        cv.put(COLUMN_PHONE, customerModel.getCustomerPhone());
         cv.put(COLUMN_ADDRESS, customerModel.getCustomerAddress());
         cv.put(COLUMN_CITY, customerModel.getCustomerCity());
-        cv.put(COLUMN_STATE_PROVINCE, customerModel.getCustomerState());
-        cv.put(COLUMN_PASSWORD_CUST, customerModel.getPassword());
+        cv.put(COLUMN_PROVINCE, customerModel.getCustomerProvince());
         // inserting data above
         long insert = db.insert(CUSTOMER_INFO, null, cv);
         return insert != -1;
     }
-
-    //adding business data to database.
-    public boolean addBusiness(BusinessModel businessModel) {
-        //writing data in the database
-        SQLiteDatabase db = this.getWritableDatabase();
-        //linking data from getters to database fields
-        ContentValues cv = new ContentValues();
-        //cv.put(COLUMN_BUSINESS_ID,businessModel.getId());
-        cv.put(COLUMN_BUSINESS_NAME, businessModel.getBusinessName());
-        cv.put(COLUMN_BUS_ADDRESS, businessModel.getBusinessAddress());
-        cv.put(COLUMN_BUS_CITY, businessModel.getBusinessCity());
-        cv.put(COLUMN_EMAIL_BUS, businessModel.getBusinessEmail());
-        cv.put(COLUMN_BUS_STATE_PROVINCE, businessModel.getBusinessState());
-        cv.put(COLUMN_PHONE_BUS, businessModel.getBusinessPhone());
-        cv.put(COLUMN_PASSWORD_BUS, businessModel.getBusPassword());
-        long insert = db.insert(BUSINESS_INFO, null, cv);
-        return insert != -1;
-    }
 }
-
 
--- app/src/main/java/com/example/smart_meal/LoginActivity.java
+++ app/src/main/java/com/example/smart_meal/LoginActivity.java
@@ -3,23 +3,24 @@ package com.example.smart_meal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     Button btnLogin, btnNewAccount;
     DBHelper DB;
     EditText email, pass;
+    String accountType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Intent intent = getIntent();
-        int typeCustomer = getIntent().getIntExtra("TYPE",-1);
 
         btnLogin = findViewById(R.id.txtLogin);
         btnNewAccount = findViewById(R.id.txtNewAccount);
@@ -34,30 +35,22 @@ public class LoginActivity extends AppCompatActivity {
                 String user = email.getText().toString();
                 String password = pass.getText().toString();
 
-                // UNCOMMENT TO TEST THE DATABASE
                 //checking if user exists and validating with db, simple validation to prevent empty fields
-//                if (user.equals("") || password.equals(""))
-//                    Toast.makeText(LoginActivityBusiness.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-//                else {
-//                    //Boolean checkBus = DB.checkBus(DBHelper.COLUMN_EMAIL_BUS);
-//                    Boolean checkUserPasswordBusiness = DB.checkUserPasswordBusiness(email.getText().toString(), pass.getText().toString());
-////                    checkUserPassword == false // add it on if when working
-//                    if (checkUserPasswordBusiness == false) { //checkBus == false ||
-//                        startActivity(new Intent(LoginActivityBusiness.this, NewAccount.class));
-//                    } else {
-//                        startActivity(new Intent(LoginActivityBusiness.this, CHomeFragment.class));
-//                    }
-//                }
-
-//                Log.d("MinhaTag","Entrou");
-                if(typeCustomer == 0){
-                    //UNCOMMENT TO TEST THE CUSTOMER CLASS
-                    startActivity(new Intent(LoginActivity.this, BusinessMain.class));
-                } else if (typeCustomer == 1){
-                    //UNCOMMENT TO TEST THE  BUSINESS CLASS
-                    startActivity(new Intent(LoginActivity.this, CustomerMain.class));
+                if (user.equals("") || password.equals(""))
+                    Toast.makeText(LoginActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else {
+                    try {
+                        Cursor cursor = DB.checkAccountType(user);
+                        accountType = cursor.getString(0);
+                        if(accountType.equals("Business")){
+                            startActivity(new Intent(LoginActivity.this, BusinessMain.class));
+                        } else if (accountType.equals("Customer")){
+                            startActivity(new Intent(LoginActivity.this, CustomerMain.class));
+                        }
+                    } catch (Exception e) {
+                        Toast.makeText(LoginActivity.this, "Account not found.", Toast.LENGTH_SHORT).show();
+                    }
                 }
-
             }
         });
 
