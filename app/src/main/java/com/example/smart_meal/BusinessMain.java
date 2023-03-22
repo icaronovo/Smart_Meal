82930e1b369dd2dd3a2a0fa5f5f18c6c64e3a04d
ericazpcalaca <ericazpc@gmail.com>
2023-03-21 19:16:40 -0700

    Insert the functional top menu on the Business Account

--- app/src/main/java/com/example/smart_meal/BusinessMain.java
+++ app/src/main/java/com/example/smart_meal/BusinessMain.java
@@ -1,14 +1,42 @@
 package com.example.smart_meal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BusinessMain extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_main);
+        //Top menu
+        toolbar = findViewById(R.id.toolbarBusiness);
+        setSupportActionBar(toolbar);
+
+    }
+    //When creating the other activities from the Business Part, if you want to put the top menu
+    //Insert all the bottom code with the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu_business,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                startActivity(new Intent(BusinessMain.this,BusinessProfile.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/smart_meal/BusinessProfile.java
@@ -0,0 +1,23 @@
+package com.example.smart_meal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class BusinessProfile extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_profile);
+
+        //Top menu
+        toolbar = findViewById(R.id.toolbarBusiness);
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_reorder_w);
+        //To do the go back option, don't forget to add this on the AndroidManifest
+        // android:parentActivityName=".BusinessMain"
+        toolbar.setTitle("Profile");
+        setSupportActionBar(toolbar);
+    }
+}
