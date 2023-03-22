e06d309e3b7ec214fb32cbb92165647701af336b
ericazpcalaca <ericazpc@gmail.com>
2023-03-21 20:47:21 -0700

    Change the icon on the New account

--- app/src/main/java/com/example/smart_meal/NewAccount.java
+++ app/src/main/java/com/example/smart_meal/NewAccount.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,7 +20,7 @@ import java.util.regex.Pattern;
 public class NewAccount extends AppCompatActivity {
     Spinner spnState;
     Button btnCreate;
-    EditText txtFName, txtLName, txtPhone, txtAddress, txtCity, txtEmail, txtPassword, txtConfirmPassword, businessName;
+    EditText username, txtPhone, txtAddress, txtCity, txtEmail, txtPassword, txtConfirmPassword;
     TextView txtErrorP, txtErrorE;
     RadioButton radioCustomer, radioBusiness;
     RadioGroup radioGroup;
@@ -32,7 +33,7 @@ public class NewAccount extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
         btnCreate = findViewById(R.id.btnCreate);
-        txtFName = findViewById(R.id.txtName);
+        username = findViewById(R.id.txtName);
         txtPhone = findViewById(R.id.txtPhone);
         txtAddress = findViewById(R.id.txtAddress);
         txtCity = findViewById(R.id.txtCity);
@@ -46,8 +47,6 @@ public class NewAccount extends AppCompatActivity {
         radioCustomer = findViewById(R.id.radioCustomer);
         radioBusiness = findViewById(R.id.radioBusiness);
         radioGroup = findViewById(R.id.radioGroup);
-        businessName = findViewById(R.id.txtBusiness);
-        businessName.setVisibility(View.GONE);
 
         //When the user select the type of account, it will change the fields required
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -55,13 +54,13 @@ public class NewAccount extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 switch (i) {
                     case R.id.radioCustomer:
-                        businessName.setVisibility(View.GONE);
-                        txtFName.setVisibility(View.VISIBLE);
+                        username.setHint("Full Name");
+                        username.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_person_outline_24, 0, 0, 0);
                         accountType = "Customer";
                         break;
                     case R.id.radioBusiness:
-                        businessName.setVisibility(View.VISIBLE);
-                        txtFName.setVisibility(View.GONE);
+                        username.setCompoundDrawablesWithIntrinsicBounds(R.drawable.shop_icon, 0, 0, 0);
+                        username.setHint("Business");
                         accountType = "Business";
                         break;
                 }
@@ -73,13 +72,7 @@ public class NewAccount extends AppCompatActivity {
             public void onClick(View v) {
                 //Get the information on the form
                 //Get the name depending of the type of account
-                String name = "";
-                if (accountType.equals("Business")){
-                    name = businessName.getText().toString();
-                }else if(accountType.equals("Customer")){
-                    name = txtFName.getText().toString();
-                }
-                String type = accountType;
+                String name = username.getText().toString();
                 String phone = txtPhone.getText().toString();
                 String address = txtAddress.getText().toString();
                 String city = txtCity.getText().toString();
@@ -90,16 +83,20 @@ public class NewAccount extends AppCompatActivity {
 
                 //constructor for  new users
                 CustomerModel customerModel ;
-//                BusinessModel businessModel ;
 
                 try {
-                        customerModel = new CustomerModel(type, email, password, name, phone, address, city, state);
+                        customerModel = new CustomerModel(accountType, email, password, name, phone, address, city, state);
                         DB.addCustomer(customerModel);
-                        if(type.equals("Customer")){
+
+                            Log.d("MyTag","na porta lol");
+
+                        if(accountType.equals("Customer")){
+                            Log.d("MyTag","ENTREI");
                             startActivity(new Intent(NewAccount.this,CustomerMain.class));
-                        } else if (type.equals("Business")){
+                        } else if (accountType.equals("Business")){
                             startActivity(new Intent(NewAccount.this,BusinessMain.class));
                         }
+
                 } catch (Exception e) {
                     if (email.isEmpty() || password.isEmpty() || name.isEmpty() || phone.isEmpty() || address.isEmpty() || city.isEmpty() || state.isEmpty()) {
                         //Check if the information is empty
